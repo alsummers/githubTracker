9598d671a189b2d8f72d6eb95b9414666fd1acc3
Allison Summers <allisonsummers44@gmail.com>
2018-11-18 15:40:54 -0500

    initial 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Row, Col} from 'reactstrap'
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faAngleUp)
+library.add(faCheckCircle)
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+     events: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.github.com/users/alsummers/events').then(response =>
+  response.json()).then((data) => {
+    this.setState({events: data})
+    console.log(this.state)
+  })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3>GitTrack</h3>
+          <h6>Events from www.github.com/alsummers</h6>
         </header>
+        <div>{this.state.events.map(event => {
+          if(event.type === "PushEvent"){
+
+        return <Row className="card-content" key={event.id}>
+        <FontAwesomeIcon  icon={faCheckCircle}/>
+          <Col>
+          <h3 className="title">{event.type === "PushEvent" ? 'Push' : "Pull Request"}</h3>
+          <p>{event.created_at.slice(0,10)}</p>
+          <p>{event.repo.name.slice(10)}</p>
+          </Col>
+        </Row>
+          } else {
+            return <Row className="card-content" key={event.id}>
+            <FontAwesomeIcon icon={faAngleUp} />
+          <Col>
+          <h3 className="title">{event.type === "PushEvent" ? 'Push' : "Pull Request"}</h3>
+          <p>{event.created_at.slice(0,10)}</p>
+          <p>{event.repo.name.slice(10)}</p>
+          </Col>
+        </Row>
+          }
+        }
+        )}
+        </div>
       </div>
     );
   }
